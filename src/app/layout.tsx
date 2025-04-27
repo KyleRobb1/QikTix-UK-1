d3bbb0029b315477e42f0d558aae3b028b9f484c
Kyle Robb <kylerobbnew@outlook.com>
2025-04-27 20:20:19 +0100

    Fix import path in root layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from '@/context/AuthContext';
+import { AuthProvider } from '../context/AuthContext';
 
 const inter = Inter({ subsets: ['latin'] });
 

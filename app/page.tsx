bba2b92fa0219fae51d0e88d59b8a4340088c22b
Kyle Robb <kylerobbnew@outlook.com>
2025-04-27 23:13:16 +0100

    fix: re-write home page file to ensure clean encoding and valid jsx for build

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FiCalendar, FiCreditCard, FiSmartphone, FiSearch } from 'react-icons/fi';
-
 import Header from '@/components/Header';
 
 export default function Home() {
